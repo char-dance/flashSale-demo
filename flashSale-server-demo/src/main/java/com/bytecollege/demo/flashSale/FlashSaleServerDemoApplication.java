f0db3792968b5bbc40418112237f21721e20925d
ruanweiqq <408775009@qq.com>
2019-12-30 11:15:16 +0800

    init project

---
+++ flashSale-server-demo/src/main/java/com/bytecollege/demo/flashSale/FlashSaleServerDemoApplication.java
@@ -0,0 +1,13 @@
+package com.bytecollege.demo.flashSale;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlashSaleServerDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlashSaleServerDemoApplication.class, args);
+	}
+
+}
---
+++ flashSale-webapp-demo/src/main/java/com/bytecollege/demo/flashSale/FlashSaleWebAppDemoApplication.java
@@ -0,0 +1,13 @@
+package com.bytecollege.demo.flashSale;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlashSaleWebAppDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlashSaleWebAppDemoApplication.class, args);
+	}
+
+}
