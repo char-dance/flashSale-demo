658c0e9310a2f08be633b121b510b90964d5c57a
ruanweiqq <408775009@qq.com>
2019-12-30 23:31:46 +0800

    dubbo is ok

--- flashSale-api-demo/src/main/java/com/bytecollege/demo/flashSale/GreetingService.java
+++ flashSale-api-demo/src/main/java/com/bytecollege/demo/flashSale/GreetingService.java
@@ -3,7 +3,7 @@ package com.bytecollege.demo.flashSale;
 public interface GreetingService {
 
 	default String greet(String name) {
-		return "Good morning!" + name;
+		return "Good Goog morning, " + name;
 	}
 
 }
--- flashSale-server-demo/src/main/java/com/bytecollege/demo/flashSale/GreetingServiceImpl.java
+++ flashSale-server-demo/src/main/java/com/bytecollege/demo/flashSale/GreetingServiceImpl.java
@@ -16,14 +16,15 @@ public class GreetingServiceImpl implements GreetingService {
 	}
 
 	private String generate(int hour) {
+		System.out.println("hour==============================="+hour);
 		String ret = "morning";
-		if (hour >= 10 && hour < 12) {
+		if (hour >= 11 && hour < 13) {
 			return "noon"; // 11-13
-		} else if (hour >= 12 && hour < 17) {
+		} else if (hour >= 13 && hour < 18) {
 			return "afternoon"; // 13-18
-		} else if (hour >= 17 && hour < 20) {
+		} else if (hour >= 18 && hour < 21) {
 			return "evening"; // 18-21
-		} else if (hour >= 21 && hour < 23) {
+		} else if (hour >= 21 && hour <= 23) {
 			return "night"; // 21-24
 		}
 		return ret;
