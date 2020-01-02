8dc7a6c20da64d24a54a10e5c91bf83a2a9c8445
ruanweiqq <408775009@qq.com>
2020-01-02 10:01:19 +0800

    init checkout system

---
+++ flashSale-service-demo/src/main/java/com/bytecollege/demo/flashSale/GreetingServiceImpl.java
@@ -0,0 +1,50 @@
+package com.bytecollege.demo.flashSale;
+
+import java.time.LocalTime;
+
+import org.apache.dubbo.config.annotation.Service;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
+
+import com.bytecollege.demo.flashSale.dao.GreetingEntity;
+import com.bytecollege.demo.flashSale.dao.GreetingMapper;
+
+@Service(version = "1.0.0", timeout = 3000)
+public class GreetingServiceImpl implements GreetingService {
+	private static final String TEMPLATE = "Good %s, %s! This is %s";
+
+	@Autowired
+	private StringRedisTemplate redisTemplate;
+
+	@Autowired
+	GreetingMapper greetingMapper;
+
+	@Override
+	public String greet(String name) {
+		redisTemplate.opsForValue().set("foo", "bar");
+		String value = redisTemplate.opsForValue().get("foo");
+		System.out.println("value======================================" + value);
+
+		GreetingEntity greetingEntity = greetingMapper.getContentById(1);
+		String content = greetingEntity.getContent();
+		System.out.println("content======================================" + content);
+
+		// 根据时间返回morning，noon,afternoon，evening，night
+		int hour = LocalTime.now().getHour();
+		return String.format(TEMPLATE, generate(hour), name, content);
+	}
+
+	private String generate(int hour) {
+		String ret = "morning";
+		if (hour >= 11 && hour < 13) {
+			return "noon"; // 11-13
+		} else if (hour >= 13 && hour < 18) {
+			return "afternoon"; // 13-18
+		} else if (hour >= 18 && hour < 21) {
+			return "evening"; // 18-21
+		} else if (hour >= 21 && hour <= 23) {
+			return "night"; // 21-24
+		}
+		return ret;
+	}
+}
