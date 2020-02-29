6a431ad1fb71cb368072404d67dca15db2241764
ruanweiqq <408775009@qq.com>
2020-02-29 21:58:52 +0800

    misc

--- flashSale-service-demo/src/main/java/com/bytecollege/demo/flashSale/FlashSaleServiceImpl.java
+++ flashSale-service-demo/src/main/java/com/bytecollege/demo/flashSale/FlashSaleServiceImpl.java
@@ -15,6 +15,7 @@ import com.bytecollege.demo.checkout.CheckoutReq;
 import com.bytecollege.demo.checkout.CheckoutResp;
 import com.bytecollege.demo.checkout.CheckoutService;
 import com.bytecollege.demo.flashSale.dao.FlashSaleMapper;
+import com.bytecollege.demo.flashSale.dao.entity.CampaignEntity;
 import com.bytecollege.demo.flashSale.dao.entity.ItemEntity;
 
 @Service(version = "1.0.0", timeout = 3000)
@@ -42,7 +43,7 @@ public class FlashSaleServiceImpl implements FlashSaleService {
 		String itemId = req.getItemId();
 		String userId = req.getUserId();
 
-		int stock = readStock(itemId);
+		int stock = checkStock(itemId);
 		if (stock <= 0) {
 			return new FlashSaleResp(itemId, userId, "NoOrder", -1, "stock is unavailable" + stock);
 		}
@@ -58,31 +59,52 @@ public class FlashSaleServiceImpl implements FlashSaleService {
 		String itemId = req.getItemId();
 		String userId = req.getUserId();
 
-		// 1.检查库存
-		int stock = readStock(itemId);
-		if (stock <= 0) {
-			return new FlashSaleResp(itemId, userId, "NoOrder", -1, "stock is unavailable" + stock);
+		// 1.检查
+		int ret = checkStatus(itemId);
+		if (ret <= 0) {
+			return new FlashSaleResp(itemId, userId, "NoOrder", -1, "stock is unavailable" + ret);
+		}
+
+		ret = checkStock(itemId);
+		if (ret <= 0) {
+			return new FlashSaleResp(itemId, userId, "NoOrder", -1, "stock is unavailable" + ret);
 		}
 
 		// 2.扣减库存
 		updateStock(itemId, 1);
 
 		// 3.下单
-		CheckoutReq checkoutReq = new CheckoutReq(itemId, userId);
-		CheckoutResp checkoutResp = checkoutService.checkout(checkoutReq);
-		log.info("========================" + checkoutReq);
-		log.info("========================" + checkoutResp);
+		CheckoutResp checkoutResp = checkout(itemId, userId);
 		if (checkoutResp.getCode() < 0) {
-			// 恢复库存
+			// 恢复库存, 或者异步补偿
 			updateStock(itemId, -1);
 			return new FlashSaleResp(itemId, userId, checkoutResp.getOrderId(), -3, checkoutResp.getMessage());
 		}
 
-		// 返回响应
 		return new FlashSaleResp(itemId, userId, checkoutResp.getOrderId(), 0, "flash sale success");
 	}
 
-	private int readStock(String itemId) {
+	private int checkStatus(String itemId) {
+		int ret = 0;
+		String status = stringRedisTemplate.opsForValue().get("status");
+		// Integer stock = flashSaleRedisTemplate.opsForValue().get(itemId);
+		log.info("status from redis======================================" + status);
+		if (status == null) {
+			CampaignEntity campaignEntity = flashSaleMapper.getCampaign(1);
+			log.info("status from mysql======================================" + campaignEntity);
+			if (campaignEntity.getStatus() == 0) {
+				ret = -1;
+			}
+
+			status = campaignEntity.getStatus() + "";
+			stringRedisTemplate.opsForValue().set("status", status);
+		}
+
+		return ret;
+	}
+
+	private int checkStock(String itemId) {
+		int ret = 0;
 		String stock = stringRedisTemplate.opsForValue().get(itemId);
 		// Integer stock = flashSaleRedisTemplate.opsForValue().get(itemId);
 		log.info("stock from redis======================================" + stock);
@@ -90,20 +112,28 @@ public class FlashSaleServiceImpl implements FlashSaleService {
 			ItemEntity itemEntity = flashSaleMapper.getItem(itemId);
 			log.info("stock from mysql======================================" + stock);
 			if (itemEntity.getStock() <= 0) {
-				return -1;
+				ret = -1;
 			}
 
 			stock = itemEntity.getStock() + "";
 			stringRedisTemplate.opsForValue().set(itemId, stock);
 		}
 
-		return Integer.parseInt(stock);
+		return ret;
 	}
 
 	private int updateStock(String itemId, int count) {
 		stringRedisTemplate.delete(itemId);
 		flashSaleMapper.updateStock(itemId, count);
 		return 0;
+	}
+
+	private CheckoutResp checkout(String itemId, String userId) {
+		CheckoutReq checkoutReq = new CheckoutReq(itemId, userId);
+		CheckoutResp checkoutResp = checkoutService.checkout(checkoutReq);
+		log.info("========================" + checkoutReq);
+		log.info("========================" + checkoutResp);
+		return checkoutResp;
 	}
 
 }
